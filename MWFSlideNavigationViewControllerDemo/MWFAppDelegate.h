8c19fa915a9c6730ae32a5e8b998f03953e74602
Meiwin Fu <meiwin@me.com>
2012-01-24 18:10:02 +0800

    Initial Commit

---
+++ MWFSlideNavigationViewControllerDemo/MWFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MWFAppDelegate.h
+//  MWFSlideNavigationViewControllerDemo
+//
+//  Created by Meiwin Fu on 24/1/12.
+//  Copyright (c) 2012 –MwF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MWFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
